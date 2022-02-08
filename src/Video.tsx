d2d095d17ad0ff2507276ae58ec8558a6dde4117
Romain Moko <33547808+romainmaucot@users.noreply.github.com>
2022-02-08 05:30:32 +0100

    first

---
+++ src/Video.tsx
@@ -0,0 +1,19 @@
+import { Composition } from 'remotion';
+import '../assets/font.css';
+import { TOTAL_DURATION } from './constants';
+import { WeatherComposition } from './WeatherComposition';
+
+const FPS = 30;
+
+export const RemotionVideo: React.FC = () => {
+    return (
+        <Composition
+            id="WeatherComposition"
+            component={WeatherComposition}
+            durationInFrames={FPS * TOTAL_DURATION}
+            fps={FPS}
+            width={1080}
+            height={1080}
+        />
+    );
+};
